290ca67ce9eabdb46d452dbdfc2f3617efea78ac
jeffreyjgong <jeffrey.gong7@gmail.com>
2022-01-28 13:38:01 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const yargs = require('yargs');
+const axios = require('axios').default;
+
+let argv = yargs.argv;
+let country = argv.country;
+
+if (country) {
+   let url = `https://restcountries.com/v3.1/name/${country}`;
+   axios.get(url, {
+
+   }).then((response) => {
+      let country = response.data[0];
+      console.log('Population: ', country.population);
+   }).catch(err => {
+      console.log(err);
+   })
+} else {
+   console.log('Please enter a country');
+}
